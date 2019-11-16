ee25ecf02250e3fe47274eb6dfaf5a593756c1e3
bbzhanshi999 <bbzhanshi999@163.com>
2019-11-16 22:57:39 +0800

    first nuxt app

---
+++ pages/list.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+      <li v-for="item in goodList" :key="item._id">
+          <h5>{{item.itemName}}</h5>
+          <h4>{{item.price}}</h4>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            goodList:[]
+        }
+    },
+    async asyncData ({app}){
+        const {data} = await app.$axios.get("/goods/list");
+        return {goodList:data};
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
